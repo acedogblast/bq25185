dd346d75b708d7b7f701f47006eaa4dcf10a85f8
acedogblast <acedogblast@gmail.com>
2025-12-01 19:09:07 -0500

    Update docs.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,12 @@
 //!
 //! This crate provide a basic interface to read the status pins and optionally set the charge enable pin of the BQ25185 standalone linear battery charger IC from Texas Insturments.
 //! See datasheet for the proper usage of this charger IC.
+//!
+//! # Features
+//! - Supports reading the stat1 and stat2 digital pins and returns a `Status` emum.
+//! - Optional charge enable pin managment.
+//! - Designed for `no_std` environments
+//! - Optional logging support via `defmt`
 
 #![cfg_attr(not(test), no_std)]
 
